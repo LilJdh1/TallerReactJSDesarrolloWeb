7259a93b504e324fb2e32bf4d8582c2106b1553d
LilJdh1 <100974428+LilJdh1@users.noreply.github.com>
2024-10-31 15:06:24 -0500

    Primer commit con la creacion del archivo y la plantilla

---
+++ listaFavoritos/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVJv39GYySh_31jgYY6q4k4sMwdRZDyS0",
+  authDomain: "listadefavoritos-6a6fd.firebaseapp.com",
+  projectId: "listadefavoritos-6a6fd",
+  storageBucket: "listadefavoritos-6a6fd.firebasestorage.app",
+  messagingSenderId: "662398076067",
+  appId: "1:662398076067:web:c07e8ef6f387fe34cbab16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
